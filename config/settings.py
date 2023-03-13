f6b380ff60119323624f346518d77d10709dc24d
Александр Агакин <agakinalexandr@gmail.com>
2023-03-13 16:33:58 +0100

    pages app with homepage

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ str(BASE_DIR.joinpath('templates')) ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
