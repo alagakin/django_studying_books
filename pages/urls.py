89a5bc03f27cd6b3a103a67a6f33680efc129a10
Александр Агакин <agakinalexandr@gmail.com>
2023-03-14 12:22:19 +0100

    page about

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import HomePageView
+from .views import HomePageView, AboutView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about')
 ]
