8318a83d0b119e854a7ae45b4f2c52d9766202d7
Александр Агакин <agakinalexandr@gmail.com>
2023-03-14 21:59:21 +0100

    allauth - tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse, resolve
 
-from accounts.forms import CustomUserCreationForm
 
 
 class CustomUserTests(TestCase):
@@ -34,3 +33,27 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+
+    username = 'user'
+    email = 'user@gmail.com'
+
+    def setUp(self) -> None:
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'account/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
