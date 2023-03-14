d3378158ad945644a68d2bf8ec5bb0f403f821c2
Александр Агакин <agakinalexandr@gmail.com>
2023-03-14 08:03:59 +0100

    test: resolving view from url

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -20,3 +21,10 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response,
                                'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepage_view(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
