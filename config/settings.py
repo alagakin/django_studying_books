2f6e8f6bf6a4366a6bf1290647bd376f5c573103
Александр Агакин <agakinalexandr@gmail.com>
2023-03-14 11:41:55 +0100

    staticfiles: css

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [(str(BASE_DIR.joinpath('static')))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
