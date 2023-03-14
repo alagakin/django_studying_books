2227c7538acbbb71aff152fc5f499f2527310e58
Александр Агакин <agakinalexandr@gmail.com>
2023-03-14 08:00:03 +0100

    setUp method for testing

--- pages/tests.py
+++ pages/tests.py
@@ -4,14 +4,19 @@ from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
 
-    def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_home_template_is_used(self):
-        response = self.client.get(reverse('home'))
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response,
+                               'Hi there! I should not be on the page.')
