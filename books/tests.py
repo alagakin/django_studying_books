8c422de5f54ecc4671d61de72f33c01774d8be3e
Александр Агакин <agakinalexandr@gmail.com>
2023-03-15 14:28:34 +0100

    reviews - test

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
 
 
 class BookTest(TestCase):
@@ -10,6 +11,18 @@ class BookTest(TestCase):
             title='Harry Potter',
             author='JK Rowling',
             price='25.00'
+        )
+
+        self.user = get_user_model().objects.create_user(
+            username='reviewer',
+            email='reviewer@gmail.com',
+            password='12345'
+        )
+
+        self.review = Review.objects.create(
+            author=self.user,
+            book=self.book,
+            review='An excellent review'
         )
 
     def test_book_listing(self):
@@ -29,4 +42,7 @@ class BookTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'An excellent review')
         self.assertTemplateUsed(response, 'books/book_detail.html')
+
+
