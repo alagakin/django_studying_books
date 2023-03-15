9836075b41d4c5056a26fb141302dccbd05128bf
Александр Агакин <agakinalexandr@gmail.com>
2023-03-15 10:36:35 +0100

    book detail page

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 
@@ -6,3 +6,9 @@ class BookListView(ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'book_list'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
