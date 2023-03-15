14b863eba887ed13ec207f8d1178c73ce1992eac
Александр Агакин <agakinalexandr@gmail.com>
2023-03-15 18:52:36 +0100

    test with permissions

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import Permission
+
 from .models import Book, Review
 
 
@@ -12,6 +14,7 @@ class BookTest(TestCase):
             author='JK Rowling',
             price='25.00'
         )
+        self.special_permission = Permission.objects.get(codename='special_status')
 
         self.user = get_user_model().objects.create_user(
             username='reviewer',
@@ -30,13 +33,24 @@ class BookTest(TestCase):
         self.assertEqual(f'{self.book.author}', 'JK Rowling')
         self.assertEqual(f'{self.book.price}', '25.00')
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_users(self):
+        self.client.login(email='reviewer@gmail.com', password='12345')
         response = self.client.get(reverse('book_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Harry Potter')
         self.assertTemplateUsed(response, 'books/book_list.html')
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_users(self):
+        self.client.logout()
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '%s?next=/books/' % (reverse('account_login')))
+        response = self.client.get('%s?next=/books/' % (reverse('account_login')))
+        self.assertContains(response, 'Log In')
+
+    def test_book_detail_view_with_permission(self):
+        self.client.login(email='reviewer@gmail.com', password='12345')
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get('/books/12345/')
         self.assertEqual(response.status_code, 200)
