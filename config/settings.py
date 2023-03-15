3f04e35771941f8aa5f1571670e96124e3f311e6
Александр Агакин <agakinalexandr@gmail.com>
2023-03-15 08:07:34 +0100

    mailtrap configured using env variables

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,11 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
 
 DEFAULT_FROM_EMAIL = "admin@djangobookstore.com"
