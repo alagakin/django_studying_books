b91c1b0bbf587d75ca4734ce81b88f6d05e51040
Александр Агакин <agakinalexandr@gmail.com>
2023-03-15 18:19:40 +0100

    login required mixin

--- books/views.py
+++ books/views.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'book_list'
+    login_url = 'account_login'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+    login_url = 'account_login'
